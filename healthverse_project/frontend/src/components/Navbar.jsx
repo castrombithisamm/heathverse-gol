551117e2dea97ffceb79454ebcc7cf02e493689d
Castro Mbithi <38522689+castrombithisamm@users.noreply.github.com>
2023-03-21 10:58:34 +0300

    made changes

--- healthverse_project/frontend/src/components/Navbar.jsx
+++ healthverse_project/frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from "react-router-dom";
 import { links } from "../data";
-import Logo from "../images/logo.png";
+import Logo from "../images/logo...png";
+import {GoThreeBars} from "react-icons/go";
 import "./navbar.css";
 
 const Navbar = () => {
@@ -10,8 +11,18 @@ const Navbar = () => {
         <Link to="/" className="logo">
           <img src={Logo} alt="Nav Logo" />
         </Link>
-        <ul className="nav__links"></ul>
-        nav buttons
+        <ul className="nav__links">
+          {links.map(({ name, path }, index) => {
+            return (
+              <li>
+                <NavLink to={path}>{name}</NavLink>
+              </li>
+            );
+          })}
+        </ul>
+        <button className="nav__toggle-btn">
+            <GoThreeBars/>
+        </button>
       </div>
     </nav>
   );
