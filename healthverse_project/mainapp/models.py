b41f3375ca9b8b470feb78d1a070dfa9f61a7e60
marymbaire <marymbugua5821@gmail.com>
2023-03-23 18:33:00 +0300

    more changes

--- healthverse_project/mainapp/models.py
+++ healthverse_project/mainapp/models.py
@@ -7,3 +7,11 @@ class Video(models.Model):
 
     def __str__(self):
         return self.caption
+    
+
+class Articles(models.Model):
+    title= models. CharField(max_length=255)
+    content= models.TextField(max_length=5000)
+    
+    def __str__(self):
+        return self.title
