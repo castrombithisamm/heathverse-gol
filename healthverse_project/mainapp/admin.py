ee22864dfa518303f197a27ff81dce16272e7210
marymbaire <marymbugua5821@gmail.com>
2023-03-23 21:39:19 +0300

    some changes adding the articles feature

--- healthverse_project/mainapp/admin.py
+++ healthverse_project/mainapp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
-from .models import Video
-admin.site.register(Video)
+# from .models import Video,Articles
+class VideoAdmin(admin.ModelAdmin):
+    list_display=('caption','video')
+admin.site.register(models.Video, VideoAdmin)
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display=('title','image_tag')
+admin.site.register(models.Articles, ArticlesAdmin)
--- healthverse_project/mainapp/models.py
+++ healthverse_project/mainapp/models.py
@@ -1,4 +1,9 @@
+from statistics import mode
+from turtle import title
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from django.utils.html import mark_safe
+
 
 # Create your models here.
 class Video(models.Model):
@@ -8,6 +13,7 @@ class Video(models.Model):
     def __str__(self):
         return self.caption
     
+<<<<<<< Updated upstream
 
 class Articles(models.Model):
     title= models. CharField(max_length=255)
@@ -15,3 +21,15 @@ class Articles(models.Model):
     
     def __str__(self):
         return self.title
+=======
+class Articles(models. Model):
+    title= models.CharField(max_length=500)
+    body= models. TextField(max_length=2000)
+    img=models.FileField(upload_to="banners/", validators=[FileExtensionValidator(['pdf', 'doc', 'svg'])] )
+
+    def __str__(self):
+        return self.title
+ 
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="20" />' % (self.img.url) )
+>>>>>>> Stashed changes
