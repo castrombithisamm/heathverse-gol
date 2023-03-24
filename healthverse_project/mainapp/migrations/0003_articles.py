2ee7e94b21419696d94d361fc1e5a9b10f6a36ec
marymbaire <marymbugua5821@gmail.com>
2023-03-24 12:33:50 +0300

    some more changes

--- healthverse_project/mainapp/migrations/0003_articles.py
+++ healthverse_project/mainapp/migrations/0003_articles.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-       
+        ('mainapp', '0002_video_description')
     ]
 
     operations = [
