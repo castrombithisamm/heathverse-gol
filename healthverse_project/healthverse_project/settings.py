0cbc626a87afb1b045f972c9a6fe68c46ae5b7df
Castro Mbithi <38522689+castrombithisamm@users.noreply.github.com>
2023-04-17 06:36:49 +0300

    add _psycopg2

--- healthverse_project/healthverse_project/settings.py
+++ healthverse_project/healthverse_project/settings.py
@@ -138,7 +138,7 @@ WSGI_APPLICATION = 'healthverse_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresqll_psycopg2',
         'NAME': os.environ.get('DBNAME'),
         'HOST': os.environ.get('DBHOST'),
         'USER': os.environ.get('DBUSER'),
